ba5b6587571da4154b5db9740800860793f01217
Daniel Guzman <dguzman@creasys.cl>
2022-02-04 20:45:57 -0300

    sleep 100

--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -13,9 +13,9 @@ def call(){
             sh './gradlew sonarqube -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build'
         }
     }
-    stage("Paso 3: Curl Springboot Gradle sleep 60"){
+    stage("Paso 3: Curl Springboot Gradle sleep 100"){
         sh "gradle bootRun&"
-        sh "sleep 60 && curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
+        sh "sleep 100 && curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
     }
     stage("Paso 4: Subir Nexus"){
         nexusPublisher nexusInstanceId: 'nexus',
